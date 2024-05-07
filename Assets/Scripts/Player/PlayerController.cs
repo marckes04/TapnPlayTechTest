e3a6e6cde86a877b9d8b743671c69430e6ccf1a3
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2024-05-07 18:22:06 -0500

    levelDesign Added

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,16 +8,53 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed;
-    private Vector2 move;
+    private Vector2 move,mouseLook, joystickLook;
+    private Vector3 rotationTarget;
+    public bool isPc;
+
 
     public void OnMoving(InputAction.CallbackContext context)
     {
         move = context.ReadValue<Vector2>();
     }
 
+    public void OnMouseLook(InputAction.CallbackContext context)
+    {
+        mouseLook = context.ReadValue<Vector2>();
+    }
+
+
+    public void OnJoystickLook(InputAction.CallbackContext context)
+    {
+        joystickLook = context.ReadValue<Vector2>();
+    }
+
     void Update()
     {
-        movePlayer();
+        if (isPc) 
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(mouseLook);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                rotationTarget = hit.point;
+            }
+
+            movePlayerWithAim();
+        }
+        else
+        {
+          if(joystickLook.x == 0 &&  joystickLook.y == 0)
+            {
+                movePlayer();
+            }
+            else
+            {
+                movePlayerWithAim();
+            }
+        }
+       
     }
 
 
@@ -25,9 +62,42 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(move.x, 0f, move.y);
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f); 
+        if(movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+        }
 
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
     }
 
+
+    public void movePlayerWithAim()
+    {
+        if(isPc)
+        {
+            var lookPos = rotationTarget - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+
+            Vector3 aimDirection = new Vector3(rotationTarget.x, 0f, rotationTarget.z);
+
+            if(aimDirection != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.15f);
+            }
+        }
+        else
+        {
+            Vector3 aimDirection = new Vector3(joystickLook.x, 0f, joystickLook.y);
+
+            if(aimDirection != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(aimDirection),0.15f);
+            }
+        }
+
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
+
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
 }
